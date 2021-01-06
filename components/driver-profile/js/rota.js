e2891de8366636472bbcc34cc6129f1c7489b602
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2021-01-06 20:58:30 +0700

    version 2.1 commit

--- components/driver-profile/js/rota.js
+++ components/driver-profile/js/rota.js
@@ -14,3 +14,14 @@ function openReschedule() {
 function closeReschedule() {
   reschedule.style.display = 'none';
 };
+//Chat
+let darkLayer = document.querySelector('#dark-layer');
+let chat = document.querySelector('#chat');
+function openChat() {
+  darkLayer.style.display = "block";
+  chat.style.display = "block";
+};
+function closeChat() {
+  darkLayer.style.display = "none";
+  chat.style.display = "none";
+};
--- components/driver-profile/js/slider.js
+++ components/driver-profile/js/slider.js
@@ -1,8 +1,18 @@
 $('.owl-carousel').owlCarousel({
-  autoWidth:true,
   loop:false,
   dots:false,
   autoplay:false,
   margin:12,
   items:1,
 });
+
+const currentWeekContainer = document.querySelector('.current-week');
+const nextWeekContainer = document.querySelector('.shift-week');
+function nextWeek() {
+  currentWeekContainer.classList.add('hide-current-week');
+  nextWeekContainer.classList.add('show-shift-week');
+};
+function currentWeek() {
+  currentWeekContainer.classList.remove('hide-current-week');
+  nextWeekContainer.classList.remove('show-shift-week');
+};
