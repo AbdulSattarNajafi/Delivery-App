eb32a0edd8589eaf2d3221f9faac9ffbfd95a4d9
AbdulSattarNajafi <66928318+AbdulSattarNajafi@users.noreply.github.com>
2021-01-12 02:11:29 +0700

    changed managerpost

--- components/js/managerpost.js
+++ components/js/managerpost.js
@@ -26,14 +26,3 @@ function swipe(event) {
     userIdentity.style.visibility = 'visible';
   }
 }
-//Chat
-let darkLayer = document.querySelector('#dark-layer');
-let chat = document.querySelector('#chat');
-function openChat() {
-  darkLayer.style.display = "block";
-  chat.style.display = "block";
-};
-function closeChat() {
-  darkLayer.style.display = "none";
-  chat.style.display = "none";
-};
